3f91aff61513316c0377fc829805c330cd7af769
Jakub Szrama <kubuszrama@gmail.com>
2024-01-10 08:58:24 +0100

    WPF2

---
+++ WpfApp2/Clock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WpfApp2
+{
+    public class Clock : INotifyPropertyChanged
+    {
+        private DispatcherTimer _timer;
+
+        public string CurrentTime => DateTime.Now.ToLongTimeString();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public Clock()
+        {
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+
+            _timer.Tick += (sender, o) => PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs(nameof(CurrentTime)));
+
+            _timer.Start();
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowDataContext DC => (MainWindowDataContext)DataContext;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSubmitClicked(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(DC.UserName))
+            {
+                return;
+            }
+
+            DC.IsNameNeeded = false;
+            MessageBox.Show($"Hello {DC.UserName}!");
+        }
+    }
+}
